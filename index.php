a25cf517e1a8fd5d8853a397063ceaf4237d50cd
Slinix606 <81796026+Slinix606@users.noreply.github.com>
2026-06-15 22:08:50 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>My Home Page</title>
+</head>
+<body>
+<h1>Welcome to My Website! I am {your name}!</h1>
+<ul>
+<li><a href="index.php">HOME</a></li>
+<li><a href="/LT3/index.html">LEARNING TASK 3</a></li>
+</ul>
+</body>
+</html>
